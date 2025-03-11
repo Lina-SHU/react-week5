2a083883fe39b304f913db2ff847f4ecfa536795
LinaChen <sakura0477@gmail.com>
2025-03-11 23:31:43 +0800

    feat: create project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <>
+      <button type="button" class="btn btn-primary">Primary</button>
+      <button type="button" class="btn btn-secondary">Secondary</button>
+    </>
+  )
+}
+
+export default App
